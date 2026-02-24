f4d9406753992aeb953fcaafd732e6b48077efec
bhaug <bhaug08@gmail.com>
2026-02-24 10:17:20 -0600

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Bryce Stryker Haug  on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
